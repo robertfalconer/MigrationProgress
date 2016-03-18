7283d82df048d7ddc855c1970003c6871000e1b6
Maurice Kelly <osv-github@chatswood.org.uk>
2016-03-18 17:23:22 +0000

    Few updates

--- MigrationProgress.playground/Pages/Implementation [WIP].xcplaygroundpage/Sources/AWSEvents.swift
+++ MigrationProgress.playground/Pages/Implementation [WIP].xcplaygroundpage/Sources/AWSEvents.swift
@@ -23,17 +23,32 @@ enum RegisterMigrationEventBuilderNodes: String {
 }
 
 enum RegisterMigrationEventAttribute: String {
+    //  Common attributes
     case OriginalAppVersion = "original_app_version"
     case NewAppVersion = "new_app_version"
     case StoreName = "store_name"
     case DeviceModel = "device_model"
     case OSVersion = "os_version"
     case CorrelationID = "correlation_id"
+    //  Event-specific attributes
+    case StartTime = "start_time"
+    case EndTime = "end_time"
+    case ItemsToMigrate = "items_to_migrate"
+    case ItemsMigrated = "items_migrated"
+    case RecordsToMigrate = "records_to_migrate"
+    case RecordsMigrated = "records_migrated"
+    case ClassName = "class_name"
+    case ClassesToIndex = "classes_to_index"
+    case ClassesIndexed = "classes_indexed"
+
 }
 
-func sendEventToAWS(eventType: String, progressState: MigrationProgressState) {
-    let attributes = prepareCommonEventAttributes(progressState)
-
+func sendEventToAWS(eventType: String, progressState: MigrationProgressState, attributes: Dictionary<RegisterMigrationEventAttribute, String>) {
+    let commonAttributes = prepareCommonEventAttributes(progressState)
+    var combinedAttributes: Dictionary<RegisterMigrationEventAttribute, String> = attributes
+    for (attribute, value) in commonAttributes {
+        combinedAttributes[attribute] = value
+    }
 
     SKPAmazonMobileAnalytics.recordEventOfType(eventType, attributes: finalizeAttributes(attributes), metrics: [:])
 }
@@ -45,7 +60,7 @@ func prepareCommonEventAttributes(progressState: MigrationProgressState) -> Dict
     //    Store name
     //    iPad Model
     //    iOS Version
-    //    Correlation ID (to link related start and end events and potentially identify items that did not finish)
+    //    Correlation ID
     var attributes: Dictionary<RegisterMigrationEventAttribute, String> = [:]
     attributes[.OriginalAppVersion] = progressState.previousAppVersion!
     attributes[.NewAppVersion] = progressState.currentAppVersion!
@@ -83,12 +98,24 @@ class AWSEventMigrationProgressObserver: MigrationProgressObserver {
 
     func processingStartedEvent(progressState: MigrationProgressState) {
         let eventType = eventTypeBuilder([.RegisterMigration, .Start])
-        sendEventToAWS(eventType, progressState: progressState)
+        let startTime: NSDate
+        if progressState.mineCartStartTime != nil {
+            startTime = progressState.mineCartStartTime!
+        } else {
+            startTime = NSDate()
+        }
+
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.FullStyle
+        let startTimeString = formatter.stringFromDate(startTime)
+
+        let attributes: Dictionary<RegisterMigrationEventAttribute, String> = [.StartTime: startTimeString]
+        sendEventToAWS(eventType, progressState: progressState, attributes: attributes)
     }
 
     func processingEndedEvent(progressState: MigrationProgressState) {
         let eventType = eventTypeBuilder([.RegisterMigration, .Success])
-        sendEventToAWS(eventType, progressState: progressState)
+        sendEventToAWS(eventType, progressState: progressState, attributes: [:])
     }
 
     func itemProcessingStartedEvent(migrationItem: MigrationItem, progressState: MigrationProgressState) {
