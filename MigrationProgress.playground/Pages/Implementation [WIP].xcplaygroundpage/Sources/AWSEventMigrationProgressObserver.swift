a0d81a615625118e2c2d6c03087b7049c4004851
Maurice Kelly <osv-github@chatswood.org.uk>
2016-03-21 15:49:02 +0000

    Pull event related functions into class

--- MigrationProgress.playground/Pages/Implementation [WIP].xcplaygroundpage/Sources/AWSEventMigrationProgressObserver.swift
+++ MigrationProgress.playground/Pages/Implementation [WIP].xcplaygroundpage/Sources/AWSEventMigrationProgressObserver.swift
@@ -6,84 +6,6 @@ class SKPAmazonMobileAnalytics {
     }
 }
 
-func eventTypeBuilder(nodes: [RegisterMigrationEventBuilderNodes]) -> String {
-    return nodes.map{$0.rawValue}.joinWithSeparator(".")
-}
-
-enum RegisterMigrationEventBuilderNodes: String {
-    case RegisterMigration = "register_migration"
-    case Database = "database"
-    case Item = "item"
-    case Setup = "setup"
-    case SecondaryIndexing = "secondary_indexing"
-    case Start = "start"
-    case Success = "success"
-    case Fail = "fail"
-    case Class = "class"
-}
-
-enum RegisterMigrationEventAttribute: String {
-    //  Common attributes
-    case OriginalAppVersion = "original_app_version"
-    case NewAppVersion = "new_app_version"
-    case StoreName = "store_name"
-    case DeviceModel = "device_model"
-    case OSVersion = "os_version"
-    case CorrelationID = "correlation_id"
-    //  Event-specific attributes
-    case StartTime = "start_time"
-    case EndTime = "end_time"
-    case ItemsToMigrate = "items_to_migrate"
-    case ItemsMigrated = "items_migrated"
-    case ItemRegisteredNumber = "item_registered_number"
-    case ItemName = "item_name"
-    case RecordsToMigrate = "records_to_migrate"
-    case RecordsMigrated = "records_migrated"
-    case ClassName = "class_name"
-    case ClassesToIndex = "classes_to_index"
-    case ClassesIndexed = "classes_indexed"
-
-}
-
-func sendEventToAWS(eventType: String, progressState: MigrationProgressState, attributes: Dictionary<RegisterMigrationEventAttribute, String>) {
-    let commonAttributes = prepareCommonEventAttributes(progressState)
-    var combinedAttributes: Dictionary<RegisterMigrationEventAttribute, String> = attributes
-    for (attribute, value) in commonAttributes {
-        combinedAttributes[attribute] = value
-    }
-
-    SKPAmazonMobileAnalytics.recordEventOfType(eventType, attributes: finalizeAttributes(combinedAttributes), metrics: [:])
-}
-
-func prepareCommonEventAttributes(progressState: MigrationProgressState) -> Dictionary<RegisterMigrationEventAttribute, String> {
-    //  Record generic information for all events
-    //    Original app version
-    //    New app version
-    //    Store name
-    //    iPad Model
-    //    iOS Version
-    //    Correlation ID
-    let attributes: Dictionary<RegisterMigrationEventAttribute, String> = [
-        .OriginalAppVersion: progressState.previousAppVersion!,
-        .NewAppVersion: progressState.currentAppVersion!,
-        //    attributes[.StoreName] =
-
-        .CorrelationID: progressState.correlationId
-    ]
-
-    return attributes
-}
-
-func finalizeAttributes(attributes: Dictionary<RegisterMigrationEventAttribute, String>) -> Dictionary<String, String> {
-    var finalizedAttributes: Dictionary<String, String> = [:]
-    for (attribute, value) in attributes {
-        let stringAttribute = attribute.rawValue
-        finalizedAttributes[stringAttribute] = value
-    }
-
-    return finalizedAttributes
-}
-
 
 public class AWSEventMigrationProgressObserver: MigrationProgressObserver {
     public init() { }
@@ -105,7 +27,7 @@ public class AWSEventMigrationProgressObserver: MigrationProgressObserver {
 
     private func processingStartedEvent(progressState: MigrationProgressState) {
         let eventType = eventTypeBuilder([.RegisterMigration, .Start])
-        let attributes: Dictionary<RegisterMigrationEventAttribute, String> = [
+        let attributes: Dictionary<AWSEventAttributes, String> = [
             .StartTime: formatTime(progressState.mineCartStartTime),
             .ItemsToMigrate: String(progressState.totalMineCartItems)
         ]
@@ -121,7 +43,7 @@ public class AWSEventMigrationProgressObserver: MigrationProgressObserver {
             eventType = eventTypeBuilder([.RegisterMigration, .Fail])
         }
 
-        let attributes: Dictionary<RegisterMigrationEventAttribute, String> = [
+        let attributes: Dictionary<AWSEventAttributes, String> = [
             .EndTime: formatTime(progressState.mineCartEndTime),
             .ItemsToMigrate: String(progressState.totalMineCartItems),
             .ItemsMigrated: String(progressState.completedMineCartItems)
@@ -132,7 +54,7 @@ public class AWSEventMigrationProgressObserver: MigrationProgressObserver {
 
     private func itemProcessingStartedEvent(migrationItem: MigrationItem, progressState: MigrationProgressState) {
         let eventType = eventTypeBuilder([.RegisterMigration, .Item, .Start])
-        let attributes: Dictionary<RegisterMigrationEventAttribute, String> = [
+        let attributes: Dictionary<AWSEventAttributes, String> = [
             .StartTime: formatTime(progressState.mineCartItemStartTime),
             .RecordsToMigrate: String(progressState.totalMineCartItemRecords),
             .ItemRegisteredNumber: String(progressState.mineCareItemRegisteredNumber!),
@@ -144,13 +66,13 @@ public class AWSEventMigrationProgressObserver: MigrationProgressObserver {
 
     private func itemProcessingEndedEvent(migrationItem: MigrationItem, progressState: MigrationProgressState) {
         let eventType: String
-        if progressState.completedMineCartItems == progressState.totalMineCartItems {
+        if progressState.completedMineCartItemRecords == progressState.totalMineCartItemRecords {
             eventType = eventTypeBuilder([.RegisterMigration, .Item, .Success])
         } else {
             eventType = eventTypeBuilder([.RegisterMigration, .Item, .Fail])
         }
 
-        let attributes: Dictionary<RegisterMigrationEventAttribute, String> = [
+        let attributes: Dictionary<AWSEventAttributes, String> = [
             .EndTime: formatTime(progressState.mineCartItemEndTime),
             .RecordsToMigrate: String(progressState.totalMineCartItemRecords),
             .RecordsMigrated: String(progressState.completedMineCartItemRecords),
@@ -162,12 +84,86 @@ public class AWSEventMigrationProgressObserver: MigrationProgressObserver {
     }
 
 
-
     //  MARK: helper methods
     private func formatTime(maybeTime: NSDate?) -> String {
         let time = (maybeTime != nil) ? maybeTime! : NSDate()
         let formatter = NSDateFormatter()
         formatter.dateStyle = NSDateFormatterStyle.FullStyle
         return formatter.stringFromDate(time)
+    }
+
+    func sendEventToAWS(eventType: String, progressState: MigrationProgressState, attributes: Dictionary<AWSEventAttributes, String>) {
+        let commonAttributes = prepareCommonEventAttributes(progressState)
+        var combinedAttributes: Dictionary<AWSEventAttributes, String> = attributes
+        for (attribute, value) in commonAttributes {
+            combinedAttributes[attribute] = value
+        }
+
+        SKPAmazonMobileAnalytics.recordEventOfType(eventType, attributes: finalizeAttributes(combinedAttributes), metrics: [:])
+    }
+
+    func prepareCommonEventAttributes(progressState: MigrationProgressState) -> Dictionary<AWSEventAttributes, String> {
+        //  Record generic information for all events
+        //    Store name
+        //    iPad Model
+        //    iOS Version
+        let attributes: Dictionary<AWSEventAttributes, String> = [
+            .OriginalAppVersion: progressState.previousAppVersion!,
+            .NewAppVersion: progressState.currentAppVersion!,
+            //    attributes[.StoreName] =
+
+            .CorrelationID: progressState.correlationId
+        ]
+
+        return attributes
+    }
+
+    func finalizeAttributes(attributes: Dictionary<AWSEventAttributes, String>) -> Dictionary<String, String> {
+        var finalizedAttributes: Dictionary<String, String> = [:]
+        for (attribute, value) in attributes {
+            let stringAttribute = attribute.rawValue
+            finalizedAttributes[stringAttribute] = value
+        }
+        
+        return finalizedAttributes
+    }
+
+    func eventTypeBuilder(nodes: [AWSEventBuilderNodes]) -> String {
+        return nodes.map{$0.rawValue}.joinWithSeparator(".")
+    }
+
+    enum AWSEventBuilderNodes: String {
+        case RegisterMigration = "register_migration"
+        case Database = "database"
+        case Item = "item"
+        case Setup = "setup"
+        case SecondaryIndexing = "secondary_indexing"
+        case Start = "start"
+        case Success = "success"
+        case Fail = "fail"
+        case Class = "class"
+    }
+
+    enum AWSEventAttributes: String {
+        //  Common attributes
+        case OriginalAppVersion = "original_app_version"
+        case NewAppVersion = "new_app_version"
+        case StoreName = "store_name"
+        case DeviceModel = "device_model"
+        case OSVersion = "os_version"
+        case CorrelationID = "correlation_id"
+        //  Event-specific attributes
+        case StartTime = "start_time"
+        case EndTime = "end_time"
+        case ItemsToMigrate = "items_to_migrate"
+        case ItemsMigrated = "items_migrated"
+        case ItemRegisteredNumber = "item_registered_number"
+        case ItemName = "item_name"
+        case RecordsToMigrate = "records_to_migrate"
+        case RecordsMigrated = "records_migrated"
+        case ClassName = "class_name"
+        case ClassesToIndex = "classes_to_index"
+        case ClassesIndexed = "classes_indexed"
+        
     }
 }
